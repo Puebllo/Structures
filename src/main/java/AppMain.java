47f86626d3b8b6d71f3c53e757ab9990d4587307
Pueblo <pawel01.galas@gmail.com>
2022-06-23 23:22:27 +0200

    recursion learning

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,9 +1,10 @@
+import c1_recursion.BinaryConverter;
 import c1_recursion.BinarySearchSolver;
 import c1_recursion.SolverAbstract;
 
 public class AppMain {
     public static void main(String[] args) {
-        SolverAbstract sa = new BinarySearchSolver();
+        SolverAbstract sa = new BinaryConverter();
         sa.doSolve();
 
     }
---
+++ src/main/java/c1_recursion/BinaryConverter.java
@@ -0,0 +1,26 @@
+package c1_recursion;
+
+import java.util.Stack;
+
+public class BinaryConverter extends SolverAbstract{
+
+    int number = 13;
+    @Override
+    protected void initData() {
+        super.initData();
+    }
+
+    @Override
+    protected void solve() {
+        decToBin(number);
+    }
+
+    private void decToBin(int number) {
+        if (number !=0){
+            decToBin(number/2);
+            System.out.println(number%2);
+        }else {
+            System.out.println("0");
+        }
+    }
+}
